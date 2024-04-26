99165c29829e6ca1d7bfb7e0fa220a65ea16453d
AiConnectHub <aiconnecthub01@gmail.com>
2024-04-26 19:20:39 +0600

    Updated 26/04/24

--- f_pipeline/server/server.py
+++ f_pipeline/server/server.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
+# from flask_cors import CORS
 import util
 
 app = Flask(__name__)
+# CORS(app)
 
-
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -20,15 +22,13 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
-
-
-
-
-if __name__ == '__main__':
-    print(f'Server is running...')
-    app.run(debug=True)
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
--- f_pipeline/server/util.py
+++ f_pipeline/server/util.py
@@ -41,6 +41,7 @@ def load_saved_artifacts():
 
         # Path to model.pickle
         model_pickle_path = os.path.join(current_dir, 'artifacts', 'model.pickle')
+        global __model
         with open(model_pickle_path, 'rb') as f:
             __model = pickle.load(f)
 
@@ -57,7 +58,7 @@ def get_data_columns():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
     print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
     print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
