486abbb3b3b136fe3d1389faf9ed509cba1c0021
AiConnectHub <aiconnecthub01@gmail.com>
2024-04-27 13:31:13 +0600

    Updated 27/04/24

--- f_pipeline/server/server.py
+++ f_pipeline/server/server.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify
-# from flask_cors import CORS
 import util
 
 app = Flask(__name__)
-# CORS(app)
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -14,19 +12,24 @@ def get_location_names():
 
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+        estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
+        response = jsonify({
+            'estimated_price': estimated_price
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    except Exception as e:
+        error_message = str(e)
+        return jsonify({'error': error_message}), 400
 
-    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
