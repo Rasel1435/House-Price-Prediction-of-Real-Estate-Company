40b7a1b1cb02d491f93ab5024d69c161386c44df
AiConnectHub <aiconnecthub01@gmail.com>
2024-04-29 17:51:07 +0600

    updated 29/04/24

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # DATA_SOURCE = 'online'
 DATA_SOURCE = 'data/Bengaluru_House_Data.csv'
 FEATURE_GROUP_NAME = 'house_price_prediction'
-FEATURE_GROUP_DESCRIPTION = "Real State House Price Prediction"
+FEATURE_GROUP_DESCRIPTION = "Real Estate House Price Prediction"
 FEATURE_DESCRIPTIONS = [
     {"name": "Location", "description": "Total Location will be here it's str type"},
     {"name": "total_sqft", "description": "Total squree feet will be here"},
@@ -15,3 +15,4 @@ TABLE_NAME = 'house-price-prediction'
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 MODEL_NAME = 'house-price-prediction'
+DOCKER = 'cddad79d40d780e360cec50b02914b5dafb9d3a208d7b063e5feaa508c2bff82'
