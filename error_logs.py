042441c39f353df1a7020dc7f1dcd613935a76bd
AiConnectHub <aiconnecthub01@gmail.com>
2024-04-29 16:32:45 +0600

    updated 29/04/24

---
+++ error_logs.py
@@ -0,0 +1,10 @@
+import logging
+
+def configure_logger():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%d-%b(%m)-%Y %I:%M:%S'
+    )
+
+    return logging.getLogger(__name__)
