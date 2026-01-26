b39c8eae86861e5814305ec6891c0e1ce52ab2e5
rasel1435 <sheikhraselahmed19@gmail.com>
2026-01-26 20:35:29 +0600

    feature enginering added

--- src/pipelines/ETLFeaturePipeline.py
+++ src/pipelines/ETLFeaturePipeline.py
@@ -6,6 +6,7 @@ from config import DATA_SOURCE
 from components.a_load import ingest_data
 from components.b_cleaning import cleaning_data
 from components.c_feature_selection import select_features
+from components.d_feature_engineering import perform_feature_engineering
 from src.logger import configure_logger
 from src.exception import CustomException
 # Configure logger
@@ -16,10 +17,11 @@ def run_pipeline():
         logger.info("==> Pipeline process has been started...")
         loading_df = ingest_data(DATA_SOURCE)
         cleaning_df = cleaning_data(loading_df)
-        df = select_features(cleaning_df)
+        feature_selection_df = select_features(cleaning_df)
+        feature_engineering_df = perform_feature_engineering(feature_selection_df)
 
         logger.info("==> Pipeline process has been done successfully!")
-        return df
+        return feature_engineering_df
     except Exception as e:
         logger.error(f"==> Pipeline process has been failed: {e}")
         return None
